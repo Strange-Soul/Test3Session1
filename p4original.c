ab81ef17592f34a1b85dd09d21eeb73c04754a3b
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-04 15:21:39 +0000

    ..

---
+++ p4original.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int input_degree()
+{
+  
+}
+float input_x()
+{
+  
+}
+void input_coefficients(int n,float a[n])
+{
+  
+}
+float evaluate_polynomials(int n,float a[n],float x)
+{
+  
+}
+void out_put(int n,float a[n],flaot x,float result)
+{
+  
+}
+int main()
+{
+  int n=input_degree();
+  float y=input_x();
+  float x[n];
+  
+}
